c4f5c1a946f42a5793509dde81075e7ffedfdb3a
Elaina <1463967532@qq.com>
2025-05-16 10:03:56 +0000

    增加了一个简单节点与消息源码,消息现在用不了

---
+++ src/test_node/Program.cs
@@ -0,0 +1,9 @@
+﻿using Rcl;
+using Rcl.Logging;
+// using Rosidl
+await using var context = new RclContext(args);
+using var node = context.CreateNode("my_ros2_node");
+node.Parameters.Declare("pub_name", "test");
+String name = node.Parameters.Get("pub_name").AsString();
+// node.CreatePublisher<r>
+node.Logger.LogInformation($"Hello ROS 2 {RosEnvironment.Distribution}!");
